983c6c75c8ee64fae424567af55416ec8152b86e
0923frisk-max <0923frisk@gmail.com>
2026-09-06 13:04:13 +0800

    Meta of Train + Test

--- Coding Environment/traffic sign classification 3.py
+++ Coding Environment/traffic sign classification 3.py
@@ -1,3 +1,10 @@
-# Coding Work - Law Yen Chang
+# Import the specific functions you need from the (renamed) file
+from color_shape.yellow_segmentation import get_images_from_directory, extract_and_crop_yellow_object
 
+# Load the list of images
+train_images = get_images_from_directory(r"./dataset/TRAIN/45")
 
+# Iterate through the list and process each image individually
+for img in train_images:
+    cropped_img, mask = extract_and_crop_yellow_object(img)
+    # Add your displaying or saving logic here
